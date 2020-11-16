fd7b8ef14f7057df133dced6358e9b4ee3bf0100
lovegroa <66517776+lovegroa@users.noreply.github.com>
2020-11-16 16:50:42 +0000

    Update database.py

--- src/common/database.py
+++ src/common/database.py
@@ -5,7 +5,9 @@ import os
 class Database(object):
     #URI = "mongodb://lovegroa:Terr0r58!@ds029969.mlab.com:29969/heroku_53p67mrn"
     #URI = "mongodb://lovegroa:Terr0r58!@cluster0-shard-00-02.bvcvs.mongodb.net:27017/heroku_53p67mrn"
-    URI = "mongodb+srv://lovegroa:Terr0r58!@cluster0.bvcvs.mongodb.net/heroku_53p67mrn?retryWrites=true&w=majority"
+    #URI = "mongodb+srv://lovegroa:Terr0r58!@cluster0.bvcvs.mongodb.net/heroku_53p67mrn?retryWrites=true&w=majority"
+    URI = "mongodb://lovegroa:Terr0r58!@cluster0-shard-00-00.bvcvs.mongodb.net:27017/heroku_53p67mrn?ssl=true&replicaSet=atlas-dec4aj-shard-0&authSource=admin&retryWrites=true&w=majority"
+
     DATABASE = None
 
     @staticmethod
